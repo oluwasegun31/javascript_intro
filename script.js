87c81241866fa843908b9e95fb7dd8cad037223e
Oluwasegun Olowosoyo <105318972+oluwasegun31@users.noreply.github.com>
2022-06-22 09:00:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// javascript code that prints name, height and country //
+const myName = 'Olowosoyo Oluwasegun'
+const height = '6.0ft'
+const country = 'Nigeria'
+
+console.log(myName);
+console.log(height);
+console.log(country);
